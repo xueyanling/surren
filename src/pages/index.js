e1b249a24e9ce91e1c9e530b7410a950dd1851a9
xueyanling <xyl1500751432@163.com>
2019-07-18 09:24:26 +0800

    7.18

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import styles from './index.css';
-
+import Redirect from 'umi/redirect';
 
 export default function() {
   return (
     <div className={styles.normal}>
+      <Redirect to='/situation'/>
       <div className={styles.welcome} />
       <ul className={styles.list}>
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
